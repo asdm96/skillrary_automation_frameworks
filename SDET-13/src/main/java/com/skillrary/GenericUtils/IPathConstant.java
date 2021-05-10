0c6af57b1a6c1cfd151fca792574a713cc761836
asd <asd@Adarsh>
2021-05-10 20:31:44 +0530

    push

---
+++ SDET-13/src/main/java/com/skillrary/GenericUtils/IPathConstant.java
@@ -0,0 +1,12 @@
+package com.skillrary.GenericUtils;
+/**
+ * This interface contains file path which is required in framework
+ * @author Nitheesha
+ *
+ */
+public interface IPathConstant {
+	String PROPERTY_FILEPATH="";
+	String JSONFILEPATH="";
+	String EXCELPATH="";
+	String htmlPath="./extentReport"+JavaUtility.getCurrentSystemDate()+".html";
+}
